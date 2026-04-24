05f21f798dc83b834fd282a2da6b98abf63c87f1
remyrmit <s4088068@student.rmit.edu.au>
2026-04-24 11:26:56 +1000

    updated the link to 'register'

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost";
+const API_BASE_URL = "http://localhost:8080";
 
 const loginForm = document.getElementById("loginForm");
 const emailInput = document.getElementById("email");
